32ca0df3c527943e609468d9a44de86109e02d88
DanShortyShort <daniya.atmawan@gmail.com>
2016-04-07 00:07:25 +0700

    updating new repo with the latest development: home fragment: layout, floating action bar

---
+++ app/src/main/java/picklenostra/picklebankapp/NotifikasiFragment.java
@@ -0,0 +1,17 @@
+package picklenostra.picklebankapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniya on 3/20/16.
+ */
+public class NotifikasiFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.notifikasi_fragment, container, false);
+    }
+}
