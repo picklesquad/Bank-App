c64fbdce8f21704524026e532c763664c966152d
xdwin <edwin.pikachu@gmail.com>
2016-04-16 22:32:48 +0700

    After debugging for 5 Hours + in satnight, List of Nasabah with Search functionality v1 bisa bekerja dengan baik

---
+++ app/src/main/java/picklenostra/picklebankapp/NasabahAdapter.java
@@ -0,0 +1,128 @@
+package picklenostra.picklebankapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Edwin on 4/14/2016.
+ */
+public class NasabahAdapter extends BaseAdapter implements Filterable {
+
+    ArrayList<NasabahModel> listNasabah;
+    ArrayList<NasabahModel> defaultListNasabah;//Data list yang tidak akan dirubah
+    Activity activity;
+    Filter nasabahFilter;
+
+    public NasabahAdapter(Activity activity, ArrayList<NasabahModel> listNasabah){
+        this.activity = activity;
+        this.listNasabah = listNasabah;
+        this.defaultListNasabah = listNasabah;
+    }
+
+    @Override
+    public int getCount() {
+        return listNasabah.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listNasabah.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        ViewHolder holder = null;
+
+        if (view == null){
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.nasabah_listviewitem,null);
+            holder.nasabahName = (TextView)view.findViewById(R.id.nasabah_name);
+            holder.nasabahPhoto = (ImageView)view.findViewById(R.id.nasabah_photo);
+            view.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder)view.getTag();
+        }
+
+        NasabahModel nasabah = (NasabahModel)getItem(position);
+        holder.nasabahName.setText(nasabah.getNama());
+        if(nasabah.getPhotoUrl() == null || nasabah.getPhotoUrl().equals(""))
+            holder.nasabahPhoto.setImageResource(R.mipmap.ic_user);
+        else
+            Picasso.with(activity).load(nasabah.getPhotoUrl()).into(holder.nasabahPhoto);
+
+        return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (nasabahFilter == null){
+            nasabahFilter = new NasabahFilter();
+        }
+        return nasabahFilter;
+    }
+
+    //Search Functionality
+    private class NasabahFilter extends Filter{
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+
+            if (constraint == null || constraint.length() <= 1){
+                    filterResults.values = defaultListNasabah;
+                    filterResults.count = defaultListNasabah.size();
+            }
+            else{
+                ArrayList<NasabahModel> listNasabahFiltered = new ArrayList<>();
+                for (NasabahModel n : listNasabah){
+                    if (n.getNama().toLowerCase().contains(constraint.toString().toLowerCase()))
+                        listNasabahFiltered.add(n);
+                }
+                filterResults.values = listNasabahFiltered;
+                filterResults.count = listNasabahFiltered.size();
+            }
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if (results.count == 0){
+               notifyDataSetInvalidated();
+            }
+            else{
+                listNasabah = (ArrayList<NasabahModel>) results.values;
+                NasabahAdapter.this.notifyDataSetChanged();
+            }
+        }
+
+    }
+
+    static class ViewHolder{
+        ImageView nasabahPhoto;
+        TextView nasabahName;
+    }
+}
--- app/src/main/java/picklenostra/picklebankapp/NasabahFragment.java
+++ app/src/main/java/picklenostra/picklebankapp/NasabahFragment.java
@@ -2,17 +2,101 @@ package picklenostra.picklebankapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Daniya on 3/20/16.
  */
 public class NasabahFragment extends Fragment{
+
+    private ListView listView;
+    private EditText searchInput;
+    private String url = "http://private-ba5008-picklesquad.apiary-mock.com/bank/%1$s/nasabah/";
+    private ArrayList<NasabahModel> listNasabah;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.nasabah_fragment, container, false);
+        View view = inflater.inflate(R.layout.nasabah_fragment ,container,false);
+
+        //Instansiasi
+        listView = (ListView)view.findViewById(R.id.lv_nasabah);
+        searchInput = (EditText)view.findViewById(R.id.search_nasabah);
+        listNasabah = new ArrayList<>();
+
+        volleyRequest("1");//Ini harus diambil dari id bank
+        final NasabahAdapter adapter = new NasabahAdapter(getActivity(), listNasabah);
+        listView.setAdapter(adapter);
+
+        //Search Functionality
+        searchInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                adapter.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void volleyRequest(String idBank){
+        String params = String.format(url,idBank);
+        final StringRequest request = new StringRequest(Request.Method.GET, params, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject responseAPI = new JSONObject(response);
+                    JSONArray arrayNasabah = responseAPI.getJSONArray("nasabah");
+                    for(int i = 0; i < arrayNasabah.length(); i++){
+                        NasabahModel nasabahModel = new NasabahModel();
+                        JSONObject objectNasabah = arrayNasabah.getJSONObject(i);
+                        String id = objectNasabah.getString("id");
+                        String nama = objectNasabah.getString("name");
+                        String location = objectNasabah.getString("location");
+                        nasabahModel.setId(id);
+                        nasabahModel.setNama(nama);
+                        nasabahModel.setLokasi(location);
+                        listNasabah.add(nasabahModel);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        VolleyController.getInstance().addToRequestQueue(request);
     }
 
 }
