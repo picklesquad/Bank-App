8dd8e689d76123fc98d51cf76474845f74e198f6
Andri Kurniawan <andrikurniawan.id@gmail.com>
2016-05-13 20:34:05 +0700

    add fabric.io configure to bank app

--- app/src/main/java/picklenostra/picklebankapp/Helper/VolleyController.java
+++ app/src/main/java/picklenostra/picklebankapp/Helper/VolleyController.java
@@ -7,6 +7,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Edwin on 4/13/2016.
@@ -24,6 +26,7 @@ public class VolleyController extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         mInstance = this;
     }
 
