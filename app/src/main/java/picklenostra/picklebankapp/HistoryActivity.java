f6d688d387cefd85d542e0c3a01737bd4fc7e907
xdwin <edwin.pikachu@gmail.com>
2016-05-14 20:16:35 +0700

    Update Gradle to 2.1.0

---
+++ app/src/main/java/picklenostra/picklebankapp/HistoryActivity.java
@@ -0,0 +1,15 @@
+package picklenostra.picklebankapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+
+    }
+}
