47872984a3a53cd78218d251b0a0a3de881fdaeb
xdwin <edwin.pikachu@gmail.com>
2016-05-14 22:43:06 +0700

    Withdrawal UI

--- app/src/main/java/picklenostra/picklebankapp/TransaksiFragment.java
+++ app/src/main/java/picklenostra/picklebankapp/TransaksiFragment.java
@@ -6,15 +6,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import picklenostra.picklebankapp.Adapter.ItemTransaksiAdapter;
+import picklenostra.picklebankapp.Model.ItemTransaksiModel;
 
 
 public class TransaksiFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayList<ItemTransaksiModel> listItemTransaksiModel;
+    private String URL = "";
+    private ItemTransaksiAdapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.transaksi_fragment, container, false);
+        View view = inflater.inflate(R.layout.transaksi_fragment, container, false);
+
+        listView = (ListView)view.findViewById(R.id.transaksi_listview);
+        listItemTransaksiModel = new ArrayList<>();
+
+        adapter = new ItemTransaksiAdapter(this.getActivity(), listItemTransaksiModel);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    private void volleyRequest(){
+
     }
 
 }
